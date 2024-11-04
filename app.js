8952016852958189e4d6458cc073fee938eb2a59
Ishwar Sahani <ishwar.sahani119@gmail.com>
2024-11-04 14:30:46 +0530

    first change in app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Home Page")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("Contact Page")
+})
+
+app.get("/about", (req, res) => {
+    res.send("About Page")
+})
+
+app.listen(3000)
